bddaa13cd3dc13ea9a7bfd0cbfe7fc43a5ea3802
Ashiq Sabith <ashiqsabith328@gmail.com>
2023-12-28 19:08:44 +0530

    logger moved to this repo

---
+++ log/logger.go
@@ -0,0 +1,25 @@
+package logs
+
+import (
+	"log"
+	"os"
+)
+
+var GenLog *log.Logger
+var ErrLog *log.Logger
+
+func InitLogger() error {
+
+	generalLog, err := os.OpenFile("./pkg/log/logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	GenLog = log.New(generalLog, "General Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrLog = log.New(generalLog, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	GenLog.Println("\n\n\nApplication restarted ---------")
+
+	return nil
+
+}
