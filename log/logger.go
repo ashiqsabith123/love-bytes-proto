32e597df45de657730ef253f9670ae98ec1f742b
Ashiq Sabith <ashiqsabith328@gmail.com>
2023-12-29 09:36:17 +0530

    create folder thing added to logger

--- log/logger.go
+++ log/logger.go
@@ -3,12 +3,20 @@ package logs
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var GenLog *log.Logger
 var ErrLog *log.Logger
 
 func InitLogger(filePath string) error {
+
+	dir := filepath.Dir(filePath)
+
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return err
+	}
 
 	generalLog, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
